2dc1461e42f95f6baa971ae694d1676101c9f68a
Yhomarth <y.reyes@siv.gov.do>
2018-03-20 07:44:01 -0400

    completado el tutorial hasta el capitulo 14

---
+++ Sistema/Data/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using Sistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+
+        public static void Initialize(SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //buscar si existen registros en la tabla categoria
+
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+
+            var categorias = new Categoria[]
+                {
+
+                    new Categoria{ Nombre = "Programacion", Descripcion="Curso programacion", Estado = true},
+                    new Categoria{ Nombre = "Diseño Grafico", Descripcion="Curso Diseño grafico", Estado = true}
+                };
+
+
+            foreach (Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+
+
+            context.SaveChanges();
+
+
+
+        }
+
+    }
+}
